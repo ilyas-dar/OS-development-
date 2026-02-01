e71bf745d11c3681539322fc2237051165a8a793
Mohammad Ilyas Dar <146307537+ilyas-dar@users.noreply.github.com>
2026-02-01 17:25:26 +0530

    Delete kernel directory to restucture the repo

--- kernel/entry.asm
+++
@@ -1,7 +0,0 @@
-bits 32
-global kernel_entry
-extern kernel_main
-
-kernel_entry:
-    call kernel_main
-    jmp $
--- kernel/keyboard.asm
+++
@@ -1,9 +0,0 @@
-bits 32
-global keyboard_isr
-extern keyboard_handler
-
-keyboard_isr:
-    pusha
-    call keyboard_handler
-    popa
-    iretd
